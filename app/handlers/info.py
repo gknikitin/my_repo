566af7b8fc6eed3384006f388462321114a01a7b
gknikitin <50626285+gknikitin@users.noreply.github.com>
2022-07-29 13:57:20 +0300

    Delete app directory

--- app/handlers/info.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-from bot import dp
-
-@dp.message_handler(commands="info")
-async def info(message: types.Message):
-    await message.answer("Чтобы посмотреть активные подписки или создать новые используйте команду /follows\n"
-                         "Чтобы отписаться от всех подписок разом используйте команду /stop \n"
-                         "Чтобы вывести список автомобилей по подписке используйте команду /avito")
-
